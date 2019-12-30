2ab75720b75e2bd015ba9afb163422a3a54ff80e
liyongjie <liyongjie@tvflnet.com>
2019-12-30 15:28:22 +0800

    环境变量名修改

--- src/common/common.js
+++ src/common/common.js
@@ -1,19 +1,17 @@
 import { deepFreeze } from './utils'
 
-let isProd = process.env.NODE_ENV === 'production'
-
-export const baseUrl = process.env.VUE_APP_baseUrl
-export const webService = process.env.VUE_APP_webService
+export const baseUrl = process.env.baseUrl
+export const webService = process.env.webService
 
 export const api = {
     upload: webService + '/upload/normal/file',
-    joinMeet:'/meeting/meet',
+    joinMeet: '/meeting/meet',
 }
 export const fbId = {
     upload: 'F1912100'
 }
 
-const socketUrl=process.env.VUE_APP_socketUrl
+const socketUrl = process.env.socketUrl
 
 // 非响应式数据
 export default deepFreeze({
