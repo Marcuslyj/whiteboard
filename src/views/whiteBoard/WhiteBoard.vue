69a0f1ba1fbe749945cf9259b016dc72dab9c823
liyongjie <liyongjie@tvflnet.com>
2020-01-16 17:50:35 +0800

    第一次打开文档，添加特殊组件之后，重新初始化画板

--- src/views/whiteBoard/WhiteBoard.vue
+++ src/views/whiteBoard/WhiteBoard.vue
@@ -326,6 +326,8 @@ export default {
         // 添加特殊组件
         this.addSpecialComponent()
         console.log('add special component')
+        // 重新进入
+        this.$globalConf.toggleRouter = !this.$globalConf.toggleRouter
         // const params = {
         //   meetingId: this.$globalConf.meetingId,
         //   whiteboardId: this.$globalConf.whiteboardId,
